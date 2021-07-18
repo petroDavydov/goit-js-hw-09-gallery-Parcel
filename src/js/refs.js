1ce35d22f94970402befeb3d75951a22510900b9
petroDavydov <petrodavydov@gmail.com>
2021-07-18 11:57:59 +0300

    add file refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  mainGallery: document.querySelector(".js-gallery"),
+  lightBoxEl: document.querySelector(".js-lightbox"),
+  modal: document.querySelector(".lightbox__content"),
+  lightboxImage: document.querySelector(".lightbox__image"),
+  btnLightBox: document.querySelector('[data-action="close-lightbox"]'),
+  overlayEl: document.querySelector(".lightbox__overlay"),
+};
