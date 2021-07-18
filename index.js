6f6b7450c158d832f0fd178061b82de116a9da63
petroDavydov <petrodavydov@gmail.com>
2021-07-18 13:35:47 +0300

    return at fist

--- index.js
+++
@@ -1,6 +0,0 @@
-import { galleryItems } from "./src/js/mainobject";
-import { refs } from "./src/js/refs";
-import * as file from "./src/js/markup";
-import * as keyboard from "./src/js/keyboardArrow";
-import * as funcMain from "./src/js/function";
-import * as listen from "./src/js/listener";
