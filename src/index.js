a4c56c500a3356927cf00ef72f359628bf39194f
petroDavydov <petrodavydov@gmail.com>
2021-07-18 11:55:40 +0300

    change index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./js/mainobject";
+import { refs } from "./js/refs";
+import * as file from "./js/markup";
+import * as keyboard from "./js/keyboardArrow";
+import * as funcMain from "./js/function";
+import * as listen from "./js/listener";
