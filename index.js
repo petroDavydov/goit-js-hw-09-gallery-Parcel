8a5cf9d7a3c54a2686510572a2af74e92be78ab5
petroDavydov <petrodavydov@gmail.com>
2021-07-18 13:03:45 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./src/js/mainobject";
+import { refs } from "./src/js/refs";
+import * as file from "./src/js/markup";
+import * as keyboard from "./src/js/keyboardArrow";
+import * as funcMain from "./src/js/function";
+import * as listen from "./src/js/listener";
